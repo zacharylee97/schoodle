33a93343d4aa9549297c24619e332aef773a5ed1
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-17 14:45:15 -0400

    Added the .onDelete('cascade') function to the foreign keys in events_attendees migration file

--- db/migrations/20181017125845_add_events_attendees.js
+++ db/migrations/20181017125845_add_events_attendees.js
@@ -4,8 +4,8 @@ exports.up = function (knex, Promise) {
     knex.schema.createTable('events_attendees', function (table) {
       table.integer('events_id');
       table.integer('attendees_id');
-      table.foreign('events_id').references('id').inTable('events');
-      table.foreign('attendees_id').references('id').inTable('attendees');
+      table.foreign('events_id').references('id').inTable('events').onDelete('cascade');
+      table.foreign('attendees_id').references('id').inTable('attendees').onDelete('cascade');
       table.datetime('time_start');
       table.datetime('time_end');
       table.timestamps(true, true);
