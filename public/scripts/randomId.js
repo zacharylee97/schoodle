2870c76f56593a558798d949147f6bffd58e4d15
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-17 13:33:41 -0400

    Changed the comments of the randomId.js file

--- public/scripts/randomId.js
+++ public/scripts/randomId.js
@@ -1,4 +1,5 @@
 // Generates a URL of lengthURL; length with each character being one of 64 possibilities
+// Based on youtube's random video id generator
 module.exports = function generateRandomString(lengthURL) {
   const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_";
   let randomURL = "";
