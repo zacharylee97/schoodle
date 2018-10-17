a3b00b98a416469816b5f6b7384b7925fc9c7e9e
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-17 12:50:22 -0400

    Added the init migration file for the database

---
+++ db/migrations/20181017124150_create_scoodle_db.js
@@ -0,0 +1,30 @@
+
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('events', function (table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('description');
+      table.string('unique_url');
+    }),
+    knex.schema.createTable('attendees', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('email');
+    }),
+    knex.schema.createTable('events_attendees', function (table) {
+      table.foreign('events_id').references('id').inTable('events');
+      table.foreign('attendees_id').references('id').inTable('attendees');
+      table.date('time_start');
+      table.date('time_end');
+    })
+  ]);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('events'),
+    knex.schema.dropTable('attendees'),
+    knex.schema.dropTable('events_attendees')
+  ]);
+};
