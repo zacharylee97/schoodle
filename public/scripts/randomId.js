7a08d543a6f50523d15cee3c598d520a249a6915
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-17 13:29:02 -0400

    Added a function that generates a random base64 url id

---
+++ public/scripts/randomId.js
@@ -0,0 +1,9 @@
+// Generates a URL of lengthURL; length with each character being one of 64 possibilities
+module.exports = function generateRandomString(lengthURL) {
+  const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_";
+  let randomURL = "";
+  for (var i = 0; i < lengthURL; i++) {
+    randomURL += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomURL;
+}
