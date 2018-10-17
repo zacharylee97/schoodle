ec0673fd40bdd52c43b3f60db8f6ce6725815822
Zachary Lee <zacharylhlee@gmail.com>
2018-10-17 22:41:53 +0000

    Restart ID from 1 for times table

--- db/seeds/03_add_times.js
+++ db/seeds/03_add_times.js
@@ -1,10 +1,11 @@
 
 exports.seed = function(knex, Promise) {
-  return knex('times')
+  return Promise.all([
+    knex.raw('ALTER SEQUENCE times_id_seq RESTART WITH 1'),
+    knex('times')
     .del()
     .then(function () {
-      return Promise.all([
-        knex('times').insert([
+        return knex('times').insert([
           { events_id: 1, time_start: '2018-08-11 18:00:00', time_end: '2018-08-11 21:00:00' },
           { events_id: 1, time_start: '2018-08-12 19:00:00', time_end: '2018-08-12 21:00:00' },
           { events_id: 1, time_start: '2018-08-11 18:00:00', time_end: '2018-08-11 21:00:00' },
@@ -14,8 +15,8 @@ exports.seed = function(knex, Promise) {
           { events_id: 3, time_start: '2018-09-15 18:00:00', time_end: '2018-09-16 03:00:00' },
           { events_id: 3, time_start: '2018-09-15 18:00:00', time_end: '2018-09-16 03:00:00' },
           { events_id: 3, time_start: '2018-09-16 18:00:00', time_end: '2018-09-17 03:00:00' },
-        ])
-      ]);
-    });
+      ])
+    })
+  ]);
 };
 
