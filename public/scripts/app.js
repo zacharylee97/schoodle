22a2b260cf851d80b52ad79652a9624f231efe40
Zachary Lee <zacharylhlee@gmail.com>
2018-10-18 16:42:53 +0000

    Adds form validation to create new event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,27 +7,38 @@ $(() => {
     }
     return randomURL;
   }
-
+  function checkForm() {
+    if ($('[name=title]').val() && $('[name=description]').val() && $('[name=name]').val() && $('[name=email]').val()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //Post to db on form submission
   $('.new-event-form').on('submit', function (e) {
     e.preventDefault();
-    let uniqueUrl = generateRandomString(12);
-    $.ajax({
-      method: "POST",
-      url: "/events",
-      data: {
-        title: $('[name=title]').val(),
-        description: $('[name=description]').val(),
-        name: $('[name=name]').val(),
-        email: $('[name=email]').val(),
-        time_start: new Date(),
-        time_end: new Date(),
-        unique_url: uniqueUrl
-      }
-    }).done(() => {
-      window.location.href = `/events/${uniqueUrl}`;
-    });;
-  })
-
+    let formValid = checkForm();
+    if (formValid) {
+      let uniqueUrl = generateRandomString(12);
+      $.ajax({
+        method: "POST",
+        url: "/events",
+        data: {
+          title: $('[name=title]').val(),
+          description: $('[name=description]').val(),
+          name: $('[name=name]').val(),
+          email: $('[name=email]').val(),
+          time_start: new Date(),
+          time_end: new Date(),
+          unique_url: uniqueUrl
+        }
+      }).done(() => {
+        window.location.href = `/events/${uniqueUrl}`;
+      });
+    } else {
+      alert("Please fill in the form!");
+    }
+  });
   //Onclick function for create-new-event button
   $("#new-event-btn").click(function () {
     window.location.href = "/events/new";
