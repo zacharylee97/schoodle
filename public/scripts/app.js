79e67f4717736076ddc11d56769e8921c68f8de9
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-18 17:02:33 -0400

    Made the calendar cells toggleable to later be sent as the selected times in the db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,14 +51,18 @@ $(() => {
       method: "GET",
       url: `/events/${uniqueURL}/info`
     })
-    .done((result) => {
-      const title = result[0].title;
-      const description = result[0].description;
-      let eventDetails =
-      `<h1>Event Details</h1>
+      .done((result) => {
+        const title = result[0].title;
+        const description = result[0].description;
+        let eventDetails =
+          `<h1>Event Details</h1>
       <p>Event title: ${title}</p>
       <p>Event description: ${description}</p>`;
-      $(".event-details").append(eventDetails);
-    })
-  }
+        $(".event-details").append(eventDetails);
+      })
+  };
+
+  $('.calendar').on('click', '.calendarCell', function () {
+    $(this).addClass('calendarSelectedCell');
+  });
 });
